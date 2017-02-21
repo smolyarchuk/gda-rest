94b07e3741469276172d70ee8bdce087309783f4
Sergey Smolyarchuk <Sergey_Smolyarchuk@epam.com>
2017-02-21 21:56:44 +0200

    add entry point to web app

--- src/main/java/com/gda/ws/Application.java
+++ src/main/java/com/gda/ws/Application.java
@@ -2,13 +2,20 @@ package com.gda.ws;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableTransactionManagement
-public class Application {
-
-    public static void main(String... args) {
+public class Application extends SpringBootServletInitializer {
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
+    }
+    
+	public static void main(String... args) {
         SpringApplication.run(Application.class, args);
     }
 
